6e6d8102a8461b0d1f6b0f3fdea60a793b397892
solszym <solszym@gmail.com>
2023-11-29 13:02:53 +0100

    -workflow

--- src/lib/firebase/firebase.js
+++ src/lib/firebase/firebase.js
@@ -2,6 +2,7 @@
 import { getAnalytics } from "firebase/analytics";
 import { deleteApp, getApp, getApps, initializeApp } from "firebase/app";
 import { getAuth, setPersistence, inMemoryPersistence } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -32,3 +33,4 @@ if (!getApps().length) {
 }
 
 export const auth = getAuth(firebaseApp);
+export const db = getFirestore(firebaseApp);
